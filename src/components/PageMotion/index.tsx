f398d09a9e9d727fbd48756f08f9c1712cb54618
kyoheimaeda <kyoheimaeda1214@gmail.com>
2025-07-13 23:06:26 +0900

    feat: pageMotion変更

--- src/components/PageMotion/index.tsx
+++ src/components/PageMotion/index.tsx
@@ -32,13 +32,48 @@ export default function PageMotion({ children }: { children: React.ReactNode }) 
       <AnimatePresence mode="wait">
         {showOverlay && (
           <motion.div
-            key="overlay"
-            className={styles.overlay}
-            initial={{ x: '100%' }}
-            animate={{ x: 0 }}
-            exit={{ x: '-100%' }}
-            transition={{ duration: 0.4, ease: 'easeInOut' }}
-          />
+            key="overlayWrap"
+            className={styles.overlayWrap}
+          >
+            <motion.div
+              key="overlay"
+              className={styles.overlay}
+              initial={{ x: '100%' }}
+              animate={{ x: 0 }}
+              exit={{ x: '-100%' }}
+              transition={{ duration: 0.8, delay: 0, ease: 'circInOut' }}
+            />
+            <motion.div
+              key="overlay2"
+              className={styles.overlay2}
+              initial={{ x: '100%' }}
+              animate={{ x: 0 }}
+              exit={{ x: '-100%' }}
+              transition={{ duration: 0.8, delay: 0.1, ease: 'circInOut' }}
+            />
+            <motion.div
+              key="overlay3"
+              className={styles.overlay3}
+              initial={{ x: '100%' }}
+              animate={{ x: 0 }}
+              exit={{ x: '-100%' }}
+              transition={{ duration: 0.8, delay: 0.2, ease: 'circInOut' }}
+            />
+            <motion.p
+              className={styles.overlayText}
+              data-text="KYOHEI MAEDA"
+              style={{
+                translateX: "-50%",
+                translateY: "-50%",
+              }}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.4, ease: 'easeInOut' }}
+            >
+              KYOHEI MAEDA
+            </motion.p>
+          </motion.div>
         )}
       </AnimatePresence>
 
