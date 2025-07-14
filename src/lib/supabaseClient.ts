c465d266b4137cba4390c019fd4677dd6a63a53c
kyoheimaeda <kyoheimaeda1214@gmail.com>
2025-07-15 00:26:47 +0900

    すぱばせ

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -4,3 +4,8 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createSupabaseClient(supabaseUrl, supabaseAnonKey);
+
+// SSRなどで使う createClient を export
+export function createClient() {
+  return createSupabaseClient(supabaseUrl, supabaseAnonKey);
+}
