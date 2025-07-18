3869114d01c88304a5b0daa8e369a02bdade9075
kyoheimaeda <kyoheimaeda1214@gmail.com>
2025-07-18 11:38:30 +0900

    next.config修正

--- next.config.ts
+++ next.config.ts
@@ -7,15 +7,14 @@ const nextConfig: NextConfig = {
     includePaths: [path.join(__dirname, 'src')],
   },
   images: {
-    domains: [
-      // ★ R2の正しい公開URLのホスト名を.env.localから取得するように修正します
-      // NEXT_PUBLIC_R2_PUBLIC_DOMAIN が "https://pub-ce560a2a95dd403da34cf9b06fa1c850.r2.dev"
-      // のように 'https://' から始まっていることを確認してください！
-      process.env.NEXT_PUBLIC_R2_PUBLIC_DOMAIN
-        ? new URL(process.env.NEXT_PUBLIC_R2_PUBLIC_DOMAIN).hostname
-        : '',
-    ].filter(Boolean), // 空文字列を除去
-    // remotePatterns もより柔軟性がありますが、まずは domains で確実に動かしましょう。
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: new URL(process.env.NEXT_PUBLIC_R2_PUBLIC_DOMAIN || '').hostname,
+        port: '',
+        pathname: '/images/**', // ★ 修正点: imagesフォルダ以下の全てのパスを許可
+      },
+    ],
   },
 };
 
